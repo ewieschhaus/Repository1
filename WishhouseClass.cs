1f4ed458350dc042dd2eba1a2fba595a346efac4
Eric Wieschhaus <wish.house@yahoo.com>
2018-07-23 12:34:33 -0500

    Adding WishhouseClass to WishhouseBranch

---
+++ WishhouseClass.cs
@@ -0,0 +1,17 @@
+public class WishhouseClass {
+
+	private string mWishhouseProperty;
+
+	public WishhouseClass() {
+		mWishhouseProperty = "";
+	}
+
+	public string WishhouseProperty {
+		get {
+			return mWishhouseProperty;
+		}
+		set {
+			mWishhouseProperty = value;
+		}
+	}
+}
