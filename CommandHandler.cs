9359c0b388751980415bad3901fdf27248ee8939
Eric Wieschhaus <eric.r.wieschhaus.ctr@mail.mil>
2018-08-29 16:16:35 -0500

    adding some c# classes that are helpful in wpf

---
+++ CommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AR_COP.Common
+{
+    /// <summary>
+    /// Handler for a Command
+    /// </summary>
+    public class CommandHandler : ICommand
+    {
+        private Action mAction;
+        private bool mCanExecute;
+
+        /// <summary>
+        /// CommandHandler constructor
+        /// </summary>
+        /// <param name="action">The action to be executed</param>
+        /// <param name="canExecute">Whether or not the action can be executed</param>
+        public CommandHandler(Action action, bool canExecute = true)
+        {
+            mAction = action;
+            mCanExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return mCanExecute && (mAction != null);
+        }
+
+        /// <summary>
+        /// Event that fires when CanExecute is changed
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Executes the Action
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            mAction();
+        }
+    }
+}
---
+++ ObservableNotifyCollection.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AR_COP.Common
+{
+    public class ObservableNotifyCollection<T> : ObservableCollection<T> where T: INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Creates a new ObservableNotifyCollection of Type T
+        /// </summary>
+        public ObservableNotifyCollection()
+            : base()
+        {
+            this.Initialize();
+        }
+
+        /// <summary>
+        /// Creates a new ObservableNotifyCollection of Type T from a preset collection of items
+        /// </summary>
+        /// <param name="collection"></param>
+        public ObservableNotifyCollection(IEnumerable<T> collection)
+            : base(collection)
+        {
+            this.Initialize();
+        }
+
+        /// <summary>
+        /// Creates a new ObservableNotifyCollection of Type T from a preset list of items
+        /// </summary>
+        /// <param name="list"></param>
+        public ObservableNotifyCollection(List<T> list)
+            : base(list)
+        {
+            this.Initialize();
+        }
+
+        /// <summary>
+        /// Initializes the ObservableNotifyCollection by subscribing to the necessary PropertyChanged events
+        /// </summary>
+        private void Initialize()
+        {
+            this.CollectionChanged += this.SubscribeToItem_PropertyChanged;
+            if (this != null)
+            {
+                foreach (T item in this)
+                {
+                    item.PropertyChanged += Item_PropertyChanged;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Manages the subscription to a PropertyChanged event on each item and handles it by forwarding to the NotifyCollectionItemPropertyChangedEvent
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SubscribeToItem_PropertyChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null)
+            {
+                foreach (T item in e.NewItems)
+                {
+                    item.PropertyChanged += Item_PropertyChanged;
+                }
+            }
+            if (e.OldItems != null)
+            {
+                foreach (T item in e.OldItems)
+                {
+                    item.PropertyChanged -= Item_PropertyChanged;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Fires the Event signifying a change on an Item within the Collection
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Item_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (NotifyCollectionItemPropertyChanged != null)
+            {
+                NotifyCollectionItemPropertyChanged(sender, new NotifyCollectionItemPropertyChangedEventArgs(e.PropertyName));
+            }
+        }
+
+        /// <summary>
+        /// The EventHandler for a NotifyCollectionItemPropertyChanged Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public delegate void NotifyCollectionItemPropertyChangedEventHandler(object sender, NotifyCollectionItemPropertyChangedEventArgs e);
+
+        /// <summary>
+        /// The Event signifying a change on an Item within the Collection
+        /// </summary>
+        public event NotifyCollectionItemPropertyChangedEventHandler NotifyCollectionItemPropertyChanged;
+
+    }
+    
+    public class NotifyCollectionItemPropertyChangedEventArgs : EventArgs {
+
+        private string mProperty;
+
+        /// <summary>
+        /// Event Args for NotifyCollectionItemPropertyChanged
+        /// </summary>
+        /// <param name="property"></param>
+        public NotifyCollectionItemPropertyChangedEventArgs(string property) {
+            mProperty = property;
+        }
+
+        /// <summary>
+        /// Name of the Property that changed on the Item within the Collection
+        /// </summary>
+        public string Property {
+            get {return mProperty; }
+        }
+    }
+
+}
